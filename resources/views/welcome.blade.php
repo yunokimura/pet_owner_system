d635248840c96b4c38ab2652bd542192aa4636c0
Kevin Saw <kevinsaw@email.com>
2026-04-12 17:34:59 +0800

    feature working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -608,7 +608,7 @@
                             </div>
                             <div>
                                 <h3 class="font-semibold text-gray-900 mb-1">Office Address</h3>
-                                <p class="text-gray-600">City Hall Compound, Dasmariñas City, Cavite</p>
+                                <p class="text-gray-600">Brgy. Langkaan 2, Sitio Buwisan, Dasmariñas City, Cavite</p>
                             </div>
                         </div>
 
@@ -620,7 +620,7 @@
                             </div>
                             <div>
                                 <h3 class="font-semibold text-gray-900 mb-1">Phone</h3>
-                                <p class="text-gray-600">(046) 123-4567</p>
+                                <p class="text-gray-600">0966-881-2010</p>
                             </div>
                         </div>
 
@@ -632,7 +632,7 @@
                             </div>
                             <div>
                                 <h3 class="font-semibold text-gray-900 mb-1">Email</h3>
-                                <p class="text-gray-600">vet@dasmarinas.gov.ph</p>
+                                <p class="text-gray-600">vetdasma@yahoo.com</p>
                             </div>
                         </div>
 
@@ -717,13 +717,13 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-primary flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                             </svg>
-                            <span class="text-gray-600 text-sm">(046) 123-4567</span>
+                            <span class="text-gray-600 text-sm">0966-881-2010</span>
                         </li>
                         <li class="flex items-center space-x-3">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-primary flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
-                            <span class="text-gray-600 text-sm">vet@dasmarinas.gov.ph</span>
+                            <span class="text-gray-600 text-sm">vetdasma@yahoo.com</span>
                         </li>
                         <li class="flex items-center space-x-3">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-primary flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
